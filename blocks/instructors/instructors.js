179c7c1f2732d9862b714a30bdf5e97c849b11ae
Mariya-Z <Mariya-Z@users.noreply.github.com>
2018-03-11 14:57:25 +0300

    shedule

---
+++ blocks/instructors/instructors.js
@@ -0,0 +1,9 @@
+$('.tabs a').on('click',function(e){
+  e.preventDefault();
+  $(this).closest('.tabs').find('.tabs__item').removeClass('tabs__item--active');
+  $(this).closest('.tabs__item').addClass('tabs__item--active');
+
+  var tabs_cnt_id = $(this).closest('.tabs').data('tabs-content-id');
+  $(tabs_cnt_id).find('.instructor__item').removeClass('instructor__item--active');
+  $(this.hash).addClass('instructor__item--active');
+});
